87dd912bd20a294dc1c2cdb7ed4dc8c2f0126cce
lc <gingeraleisthebest@gmail.com>
2023-03-09 22:24:55 -0500

    setup basic profilepage

--- src/routes/profilePg.tsx
+++ src/routes/profilePg.tsx
@@ -1,3 +1,28 @@
+import Header from "../components/header";
+import RightSideMenu from "../components/sidemenu/right";
+import Tabbar from "../components/tabbar";
+import Profile from "../components/user/profile";
+
 export default function ProfilePage(): JSX.Element {
-  return <></>;
+  return (
+    <div className="flex flex-row">
+      <div className="max-w-[600px] w-full shrink-0">
+        <div>
+          <Header path="" />
+        </div>
+
+        <div>
+          <Profile />
+        </div>
+
+        <div>
+          <Tabbar path="" />
+        </div>
+      </div>
+
+      <div>
+        <RightSideMenu path="" />
+      </div>
+    </div>
+  );
 }
